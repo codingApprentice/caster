cd25a6875fc14668f4840c3eaa67abf530d30e16
Barry O'Farrell <barry7ie@gmail.com>
2018-03-29 21:46:24 +0100

    include git more commands

--- caster/apps/mintty.py
+++ caster/apps/mintty.py
@@ -32,10 +32,14 @@ class CMDRule(MergeRule):
         "open Jpractice":   R(Text(r"cd /e/GitHub/javaPractice")+Key("enter"), rdescript="git bash: Go To javaPractice"),
 		"open my website":   R(Text(r"cd /e/GitHub/codingApprentice.github.io")+Key("enter"), rdescript="git bash: Go To javaPractice"),
 		"open macro system":   R(Text(r"cd /c/NatLink/NatLink/MacroSystem")+Key("enter"), rdescript="git bash: Go To MacroSystem"),
+		"open Sikuli folder":   R(Text(r"cd /c/Users/barry/AppData/Roaming/Sikulix/Lib")+Key("enter"), rdescript="git bash: Go To MacroSystem"),
 		
 		
 		#github commands
-		"push origin master": R(Text("git push origin master")+Key("enter"), rdescript="git bash: Go To C:"),
+		"push origin master": R(Text("git push origin master")+Key("enter"), rdescript="git bash: push to the origin of the master"),
+		"push origin": R(Text("git push origin "), rdescript="git bash: push to the origin"),
+		"check out": R(Text("git checkout "), rdescript="git bash: check out"),
+		"check out master": R(Text("git checkout master"), rdescript="git bash: check out master"),
 		"my username":      R(Text("codingApprentice")+Key("enter"), rdescript="git bash: username"),
 		"check status":     R(Text("git status")+Key("enter"), rdescript="git bash: check git status"),
 		"add all files":    R(Text(r"git add -A")+Key("enter"), rdescript="git bash: add all files to commit"),
