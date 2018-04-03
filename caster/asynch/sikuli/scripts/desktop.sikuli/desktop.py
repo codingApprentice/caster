b9f5c438f8270801f315357f4ee617bba438fa5f
Barry O'Farrell <barry7ie@gmail.com>
2018-04-03 20:25:13 +0100

    add Sikulix scripts

--- caster/asynch/sikuli/scripts/desktop.sikuli/desktop.py
+++ caster/asynch/sikuli/scripts/desktop.sikuli/desktop.py
@@ -2,3 +2,9 @@ from sikuli import *
 
 def export_photograph():
     click("1522527756710.png")
+    
+def export_minty_bash():
+    click("1522546495391.png")
+
+def export_torrent():
+    click("1522546723172.png")
